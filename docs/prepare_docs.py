6bf7517dc519a1f0789fa5217db3cb1a10d19a57
pritexx <vladon_2004@mail.ru>
2023-09-08 11:13:12 +0300

    change

--- docs/prepare_docs.py
+++ docs/prepare_docs.py
@@ -1,7 +1,7 @@
 import os
 
 def replaceLinksInFiles():
-    for root, _, fileNames in os.walk("."):
+    for root, _, fileNames in os.walk("./docs"):
         for fileName in fileNames:
             if fileName.split('.')[-1] != 'md':
                 continue
@@ -9,9 +9,8 @@ def replaceLinksInFiles():
             with open(f"{root}/{fileName}", 'r') as file:
                 content = file.read()
 
-            print(content)
             content = content.replace("/.eraser/", "/img/")
-            print(content)
+
             with open(f"{root}/{fileName}", 'w') as file:
                 file.write(content)
 
@@ -19,7 +18,7 @@ def moveImagesToImgFolder():
     if not os.path.exists("img"):
         os.mkdir("img")
 
-    for fileName in os.listdir("../.eraser"):
+    for fileName in os.listdir(".eraser"):
         os.rename(f"./.eraser/{fileName}", f"./docs/img/{fileName}")
 
 replaceLinksInFiles()
